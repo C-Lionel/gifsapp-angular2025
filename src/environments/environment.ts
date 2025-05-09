2926e9372162b0e030c12d0c5e4b480ed022641f
Lionel Cancellieri <lio.cancellieri@gmail.com>
2025-05-09 00:41:37 -0300

    Updated: GifService - interfaces, sección 7: PC

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,14 @@
 export const environment = {
-  
+
   production: true,
   companyName: 'Gifs',
   companySubname: 'App',
-  companySlogan: 'Maneja tus gifs'
+  companySlogan: 'Maneja tus gifs',
+
   // ApiKeys
+  giphyApiKey: 'hE7QH6Cde1Cg4eRjnXM5KaZXFLkElz8D',
+  giphyUrl: 'https://api.giphy.com/v1',
+
   // URLS
 
 };
