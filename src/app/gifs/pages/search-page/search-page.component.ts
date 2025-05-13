9c1cc19ac4c34530da6f372ed23035df9fdb488d
Lionel Cancellieri <lio.cancellieri@gmail.com>
2025-05-12 23:10:31 -0300

    Updated: Diseño de pantalla - Buscador de Gifs, sección 7: PC

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
-  templateUrl: './search-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [GifListComponent],
+  templateUrl: './search-page.component.html'
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+
+  gifService = inject(GifService)
+
+    onSearch( query: string ) {
+      this.gifService.searchGifs(query);
+    }
+ }
