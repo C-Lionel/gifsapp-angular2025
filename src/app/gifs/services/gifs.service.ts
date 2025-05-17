8bc0bcce510028f2c819fa84f27bbbb5a5ced19b
Lionel Cancellieri <lio.cancellieri@gmail.com>
2025-05-17 00:19:58 -0300

    Updated: Mostrar historial de búsqueda, sección 7: PC

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,7 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 
 @Injectable({providedIn: 'root'})
@@ -37,7 +37,7 @@ export class GifService {
         } )
     }
 
-    searchGifs( query: string ) {
+    searchGifs( query: string ): Observable<Gif[]> {
 
        return this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
           params: {
@@ -64,6 +64,10 @@ export class GifService {
         //     console.log({search: gifs})
         // } );
 
+    }
+
+    getHistoryGifs(query: string): Gif[] {
+        return this.searchHistory()[query] ?? [];
     }
 
 }
